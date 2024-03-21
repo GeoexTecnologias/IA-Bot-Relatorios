de5432da6a2ef254eaa9e67612c7479b424593ee
Bruno Prado <brunopradocode@gmail.com>
2024-03-21 16:49:43 -0300

    Add conversational retrieval chain and conversational retrieval chain functions

--- main/ai_model.py
+++ main/ai_model.py
@@ -5,6 +5,8 @@ from langchain.cache import SQLiteCache
 from langchain.globals import set_llm_cache
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 import pinecone
 from langchain_community.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
@@ -17,7 +19,7 @@ import random
 load_dotenv()
 
 
-def generate_query_ai(index_name, question):
+def generate_gpt4(index_name):
     pc = pinecone.Pinecone(api_key=os.environ['PINECONE_API_KEY'])
     print('index', index_name, 'selected!')
 
@@ -28,25 +30,53 @@ def generate_query_ai(index_name, question):
     llm = ChatOpenAI(model='gpt-4', temperature=0.3)
     retriever = vector_store.as_retriever(
         search_type='similarity', search_kwargs={'k': 10})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
     chain = RetrievalQA.from_chain_type(
         llm=llm, chain_type='stuff', retriever=retriever)
 
-    dialect = 'MS SQL Server'
-    cols = ['Projeto', 'Nota', 'ProjetoProgramacaoCarteira']
-    few_shot_examples = '',
-    question = question
+    return chain
 
-    prompt = prompt_template(dialect, cols, few_shot_examples, question)
 
-    result = chain.invoke(prompt)['result']
-    print(result)
+def conversational_retriever_chain(index_name):
+    pc = pinecone.Pinecone(api_key=os.environ['PINECONE_API_KEY'])
+    print('index', index_name, 'selected!')
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = Pinecone.from_existing_index(
+        index_name=index_name, embedding=embeddings)
+
+    llm = ChatOpenAI(model='gpt-4', temperature=0)
+    retriever = vector_store.as_retriever(
+        search_type='similarity', search_kwargs={'k': 5})
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=False)
+
+    # Conversational Retrieval Chain
+
+    crc = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=memory,
+        chain_type='stuff'
+    )
+    return crc
+
+
+def generate_query_ai(index_name, question):
+
+    chain = conversational_retriever_chain(index_name)
+    prompt = prompt_template(question)
+    result = chain.invoke(prompt)['answer']
+
+    if 'SQLQuery' not in result:
+        return result
+
     result = result.split('SQLQuery:')[1]
-    result = result.split('"')[1]
-
     print(result)
 
-    is_valid = validate_query(result)
-    if is_valid:
+    if validate_query(result):
         if get_query(result):
             return result
         else:
@@ -76,8 +106,8 @@ def get_query(query):
         df.to_excel(
             f"./outputs/relatorio_ia_{random_number}.xlsx", index=False)
         if len(df) > 0:
-            return True
-        return False
+            return True, df
+        return False, None
 
     except Exception as e:
         print(e)
@@ -93,9 +123,9 @@ def connect_db():
     return pymssql.connect(server, username, password, database), uri
 
 
-def prompt_template(dialeto, table_info, few_shot_examples, question):
+def prompt_template(question):
     template = """
-    Dada uma pergunta de entrada, primeiro crie uma consulta {dialeto} sintaticamente correta para ser executada, depois examine os resultados da consulta e retorne a resposta.
+    Dada uma pergunta de entrada, primeiro crie uma consulta {dialeto} sintaticamente correta para ser executada, depois examine os resultados da consulta e retorne a resposta sempre em portugues.
     Use o seguinte formato:
 
     SQLQuery: "Consulta SQL a ser executada ou resposta da pergunta."
@@ -107,14 +137,36 @@ def prompt_template(dialeto, table_info, few_shot_examples, question):
     Alguns exemplos de consultas SQL que se enquadram em perguntas são:
 
     {few_shot_examples}
-
+    
+    Observações: 
+        - Em consultas com datas e que o usuário nao especificou o ano, use o ano atual.
+        - Se o input do usuário for como uma conversa normal, seja educado e mais humano possível.
+        - Caso o usuário nao entenda, explique para ele o que voce faz e quais dados do banco voce consegue consultar
     Pergunta: {input}
     """
+    dialect = 'MS SQL Server'
+    cols = ['Projeto', 'Nota', 'ProjetoProgramacaoCarteira']
+    few_shot_examples = """
+    Pergunta: Quais projetos/obras estao programados para serem executados nas carteiras de maio até dezembro deste ano?
+    
+    SQLQuery: SELECT Projeto.Titulo 
+                FROM Projeto 
+                INNER JOIN ProjetoProgramacaoCarteira 
+                ON Projeto.ProjetoId = ProjetoProgramacaoCarteira.ProjetoId 
+                WHERE ProjetoProgramacaoCarteira.Carteira BETWEEN CONVERT(date, CONVERT(varchar(4), YEAR(GETDATE())) + '-05-01') AND CONVERT(date, CONVERT(varchar(4), YEAR(GETDATE())) + '-12-31')
+
+    
+    
+    """,
     prompt_template = PromptTemplate.from_template(template=template)
-    return prompt_template.format(dialeto=dialeto, table_info=table_info, few_shot_examples=few_shot_examples, input=question)
+    return prompt_template.format(dialeto=dialect, table_info=cols, few_shot_examples=few_shot_examples, input=question)
 
 
 if __name__ == "__main__":
-    question = input("Digite a pergunta: ")
-    query = generate_query_ai("geoex-sql-embeddings", question)
-    print(query)
+    while True:
+        question = input("Digite a pergunta: ")
+        if question == 'tchau':
+            print('Estou aqui para ajudar! Até a próxima')
+            break
+        query = generate_query_ai("geoex-sql-embeddings", question)
+        print(query)
