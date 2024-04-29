9d2bc1860636eaffef715ec2a63686d7cb26c21b
brprado <brunopradocode@gmail.com>
2024-04-29 09:53:34 -0300

    windows setup

--- AIServer/app.py
+++ AIServer/app.py
@@ -3,6 +3,8 @@ from langchain_community.llms import LlamaCpp
 from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from langchain_core.prompts import PromptTemplate
 
+# /TheBloke/Mistral-7B-Instruct-v0.1-GGUF/blob/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf
+
 template = "<s>[INST] Which Projects are booked for January?[/INST]"
 "SELECT * FROM Projeto p WHERE MONTH(p.Carteira) = 1</s> "
 "[INST] {question} [/INST]"
@@ -10,18 +12,16 @@ template = "<s>[INST] Which Projects are booked for January?[/INST]"
 
 prompt = PromptTemplate.from_template(template)
 
-# Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-# Make sure the model path is correct for your system!
 llm = LlamaCpp(
-    model_path="./models/mistral-7b-instruct-v0.1.Q2_K.gguf",
+    model_path="./models/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
     temperature=0.1,
     max_tokens=500,
     n_gpu_layers=-1,
     top_p=1,
     callback_manager=callback_manager,
-    verbose=False,  # Verbose is required to pass to the callback manager
+    verbose=False
 )
 
 question = """
