f5a002562410da1b8dc52b22aced37c5609e5307
brprado <brunopradocode@gmail.com>
2024-05-09 15:38:51 -0300

    Update Report.csv with new total projects count

--- AI_Chat/api_test.py
+++ AI_Chat/api_test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://127.0.0.1:8000/generate-response"  # replace with your server's URL
+
 data = {
     "user_question": "Quantos os projetos estao programados para a carteira de obras de maio"
 }
